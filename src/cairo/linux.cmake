57e700060e7e4a8e1609ab0c362f052ce1c6919d
livingcreative <dnkreative@gmail.com>
2016-11-21 22:26:43 +0400

    Linux cairo build

---
+++ src/cairo/linux.cmake
@@ -0,0 +1,26 @@
+#
+#      KCANVAS PROJECT
+#
+#  Common 2D graphics API abstraction with multiple back-end support
+#
+#  (c) livingcreative, 2015 - 2016
+#
+#  https://github.com/livingcreative/kcanvas
+#
+#  linux.cmake
+#      cmake include file for Linux platform build
+#
+
+# windows platform build headers
+set(HEADERS
+	${HEADERS}
+	${INCLUDE_PATH}/cairo/platform.h
+	cairo/canvasimplcairo.h
+)
+
+# windows platform build sources
+set(SOURCES
+	${SOURCES}
+	cairo/platform.cpp
+	cairo/canvasimplcairo.cpp
+)
