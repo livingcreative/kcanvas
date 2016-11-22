0ba2c185754813ac9f7d105d02e354664a320a40
livingcreative <dnkreative@gmail.com>
2016-11-22 19:20:40 +0400

    cmake add_compile_options replaced with set flags for CI support

--- src/gcc.cmake
+++ src/gcc.cmake
@@ -13,5 +13,5 @@
 
 # GCC specific defines and options
 if (CMAKE_COMPILER_IS_GNUCXX)
-	add_compile_options(-std=c++11)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 endif (CMAKE_COMPILER_IS_GNUCXX)
