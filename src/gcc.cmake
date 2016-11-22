912bf184efb8f37ea8dd8da4298ab2bc5eb4d37d
livingcreative <dnkreative@gmail.com>
2016-11-22 19:41:10 +0400

    fixes for travis ci

--- src/gcc.cmake
+++ src/gcc.cmake
@@ -13,5 +13,5 @@
 
 # GCC specific defines and options
 if (CMAKE_COMPILER_IS_GNUCXX)
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++0x")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
 endif (CMAKE_COMPILER_IS_GNUCXX)
